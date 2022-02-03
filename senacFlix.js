e71c720ff75e3e65d54f0e71cc17b40f67d4f236
SOTERO795 <edsonsotero@hotmail.com>
2022-02-03 19:54:33 -0300

    subindo o js

---
+++ senacFlix.js
@@ -0,0 +1,8 @@
+/*console.log("usuário 1: assistindo filme");
+console.lo("usuario 2: assistindo série");
+console.log("usuário 1: assistindo filme");
+console.log("usuario 2: assistindo série");*/
+setTimeout(function(){
+console.console.log();('Primeiro')   
+},3000);
+console.log('Segundo');
