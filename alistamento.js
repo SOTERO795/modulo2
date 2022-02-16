4587fbb2a79c80cf58d89ccdb7f317c471cf292c
SOTERO795 <edsonsotero@hotmail.com>
2022-02-15 21:12:21 -0300

    aula 15/02/2022 - Estudo de Arrays

--- alistamento.js
+++ alistamento.js
@@ -1,20 +1,29 @@
 const dadosDigitadoUsuario = require("readline-sync");
+
 console.log("MINISTÉRIO DA DEFESA");
 console.log("SEJA BEM VINDO A FORÇA ÁREA BRASILEIRA");
 
 const ano = dadosDigitadoUsuario.question("INFORME SEU ANO DE NASCIMENTO - ");
 
-anoNascimento = ano;
-anoAtual = 2022;
+var anoNascimento = ano;
+var anoAtual = 2022;
 
-idade = anoAtual - anoNascimento;
+var idade = anoAtual - anoNascimento;
 
+//Verifica se a pessoa tem idade inferior a 18 anos 
 if (idade <= 17) {
   console.log("VOCÊ TEM" + " " + idade + " " + "ANOS");
   console.log("ALISTAMENTO NÃO PERMITIDO");
+
+  //Verifica se a pessoa tem  apenas 18 anos de idade
 } else if (idade === 18) {
   console.log("VOCÊ TEM" + " " + idade + " " + "ANOS");
-  console.log("ALISTAMENTO PERMITIDO, APRESENTE-SE NA SALA DO COMANDO");
-} else if (idade > 18 && idade <= 19) {
-  console.log("FORA DO PRAZO DE ALISTAMENTO");
+  console.log("ALISTAMENTO PERMITIDO, APRESENTE-SE A JUNTA DE ALISTAMENTO");
+
+  //Informa que a pessoa é maior de 18 anos e orienta a situação necessária
+} else {
+  console.log("VOCÊ TEM" + " " + idade + " " + "ANOS");
+  console.log(
+    "PASSOU DO PRAZO DE ALISTAMENTO, APRESENTE-SE A JUNTA DE ALISTAMENTO E DEPOIS NA SALA DO COMANDO"
+  );
 }
---
+++ exercicioArrays.js
@@ -0,0 +1,44 @@
+/*const lista = new Array('pera', 'uva', 'maça');
+const primeiroItem = lista['']
+
+const tamanho = lista.length
+console.log(tamanho);
+//console.log(primeiroItem);*/
+
+//const cadastroAlunos = new Array('nome','matricula','curso','horario');
+
+const cadastroAlunos = [
+
+    {
+     nome: 'edson',
+     matricula:'195971',
+     curso: 'Back End',
+     horario:'16:30',
+    },
+
+    {
+     nome: 'Tereza',
+     matricula:'195972',
+     curso: 'Back End',
+     horario:'16:30',
+     },
+
+     {
+        nome: 'Paulo',
+        matricula:'195973',
+        curso: 'Back End',
+        horario:'16:30',
+     }
+]
+
+const aluno = cadastroAlunos[0];
+
+console.log(cadastroAlunos.length)
+
+console.log(cadastroAlunos[0],cadastroAlunos[1],cadastroAlunos[2]);
+
+console.log(aluno.nome),
+console.log(aluno.curso)
+
+
+
